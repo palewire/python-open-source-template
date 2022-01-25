29287a04236b805b0ef463045841fa6129c59603
palewire <b@palewi.re>
2022-01-25 10:28:30 -0800

    Tweak

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+
 from setuptools import setup
 
 
@@ -10,27 +11,27 @@ def read(file_name):
 
 
 setup(
-    name='<your-pypi-package-name>',
-    version='0.0.1',
+    name="<your-pypi-package-name>",
+    version="0.0.1",
     description="<your-repo-description>",
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    author='Ben Welsh',
-    author_email='b@palewi.re',
-    url='http://www.github.com/datadesk/<your-repo-slug>',
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Ben Welsh",
+    author_email="b@palewi.re",
+    url="http://www.github.com/datadesk/<your-repo-slug>",
     license="MIT",
     packages=("<your-python-module-name>",),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
     ],
     project_urls={
-        'Maintainer': 'https://github.com/datadesk',
-        'Source': 'https://github.com/datadesk/<your-repo-slug>',
-        'Tracker': 'https://github.com/datadesk/<your-repo-slug>/issues'
+        "Maintainer": "https://github.com/datadesk",
+        "Source": "https://github.com/datadesk/<your-repo-slug>",
+        "Tracker": "https://github.com/datadesk/<your-repo-slug>/issues",
     },
 )
--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 #! /usr/bin/env python
-# -*- coding: utf-8 -*-
 import unittest
 
 
 class MyUnitTest(unittest.TestCase):
-
     def test_foo(self):
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
