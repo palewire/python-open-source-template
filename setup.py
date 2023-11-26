916aeae1e4d5fef4e3391475dbd95434cc93b2af
Ben Welsh <b@palewi.re>
2023-11-26 15:32:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -70,5 +70,6 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
+        "Typing :: Typed",
     ],
 )
